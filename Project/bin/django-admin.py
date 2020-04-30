d4a7dd8094635c615fd81abd7f521ca8d907beda
sakeerin <theblue0800@gmail.com>
2020-04-30 13:53:00 +0800

    create project and app and setting project and app

---
+++ Project/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sakeerinkhami/Documents/python/Django-E-Commerce/Project/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
