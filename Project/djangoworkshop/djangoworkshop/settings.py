c70ba5316a25362ba3fd17d0ed2b42f3a83d3c3b
sakeerin <theblue0800@gmail.com>
2020-04-30 15:51:06 +0800

    setting static path image

--- Project/djangoworkshop/djangoworkshop/settings.py
+++ Project/djangoworkshop/djangoworkshop/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATIC_URL='/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'static','media')
+
 
 # Application definition
 
--- Project/djangoworkshop/djangoworkshop/urls.py
+++ Project/djangoworkshop/djangoworkshop/urls.py
@@ -16,9 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from store import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index),
     path('product/',views.product),
 ]
+
+if settings.DEBUG :
+    #/media/product
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    #/static
+    urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    #/static/media/product/
+
