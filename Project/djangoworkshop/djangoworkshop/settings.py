5f335c9579771f0ab4d882d028aba4590fe791d1
sakeerin <theblue0800@gmail.com>
2020-05-09 09:46:53 +0800

    setup database

--- Project/djangoworkshop/djangoworkshop/settings.py
+++ Project/djangoworkshop/djangoworkshop/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'djangoworkshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storedb',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
