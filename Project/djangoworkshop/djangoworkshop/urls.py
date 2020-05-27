c2669502bc214dc1f91f0efdad5ae33324183a03
sakeerin <theblue0800@gmail.com>
2020-05-27 16:14:53 +0800

    coding in part delete product in cartItem

--- Project/djangoworkshop/djangoworkshop/urls.py
+++ Project/djangoworkshop/djangoworkshop/urls.py
@@ -25,7 +25,8 @@ urlpatterns = [
     path('category/<slug:category_slug>',views.index,name="product_by_category"),
     path('product/<slug:category_slug>/<slug:product_slug>',views.productPage,name='productDetail'),
     path('cart/add/<int:product_id>',views.addCart,name="addCart"),
-    path('cartdetail/',views.cartdetail,name="cartdetail")
+    path('cartdetail/',views.cartdetail,name="cartdetail"),
+    path('cart/remove/<int:product_id>',views.removeCart,name="removeCart")
 ]
 
 if settings.DEBUG :
