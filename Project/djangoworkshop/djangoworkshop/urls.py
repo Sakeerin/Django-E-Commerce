a2b5e46ba749d8d280b8a863ef108dcd47b8e74e
sakeerin <theblue0800@gmail.com>
2020-06-01 08:50:15 +0800

    coding in part signIn

--- Project/djangoworkshop/djangoworkshop/urls.py
+++ Project/djangoworkshop/djangoworkshop/urls.py
@@ -21,19 +21,23 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index,name="home"),
-    path('category/<slug:category_slug>',views.index,name="product_by_category"),
-    path('product/<slug:category_slug>/<slug:product_slug>',views.productPage,name='productDetail'),
-    path('cart/add/<int:product_id>',views.addCart,name="addCart"),
-    path('cartdetail/',views.cartdetail,name="cartdetail"),
-    path('cart/remove/<int:product_id>',views.removeCart,name="removeCart"),
-    path('account/create',views.signUpView,name="signUp")
+    path('', views.index, name="home"),
+    path('category/<slug:category_slug>',
+         views.index, name="product_by_category"),
+    path('product/<slug:category_slug>/<slug:product_slug>',
+         views.productPage, name='productDetail'),
+    path('cart/add/<int:product_id>', views.addCart, name="addCart"),
+    path('cartdetail/', views.cartdetail, name="cartdetail"),
+    path('cart/remove/<int:product_id>', views.removeCart, name="removeCart"),
+    path('account/create', views.signUpView, name="signUp"),
+    path('account/login', views.signInView, name="signIn")
 ]
 
-if settings.DEBUG :
-    #/media/product
-    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-    #/static
-    urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-    #/static/media/product/
-
+if settings.DEBUG:
+    # /media/product
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    # /static
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    # /static/media/product/
