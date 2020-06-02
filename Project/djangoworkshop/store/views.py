375b0dd7dc2c3e5db66c0d4104aa0a2d6add39af
sakeerin <theblue0800@gmail.com>
2020-06-02 12:14:20 +0800

    coding in part pagination

--- Project/djangoworkshop/store/views.py
+++ Project/djangoworkshop/store/views.py
@@ -4,6 +4,7 @@ from store.forms import SignUpForm
 from django.contrib.auth.models import Group, User
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate, logout
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
 
 
 # Create your views here.
@@ -17,7 +18,18 @@ def index(request, category_slug=None):
     else:
         products = Product.objects.all().filter(available=True)
 
-    return render(request, 'index.html', {'products': products, 'category': category_page})
+    paginator = Paginator(products, 3)
+    try:
+        page = int(request.GET.get('page', '1'))
+    except:
+        page = 1
+
+    try:
+        productperPage = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        productperPage = paginator.page(paginator.num_pages)
+
+    return render(request, 'index.html', {'products': productperPage, 'category': category_page})
 
 
 def productPage(request, category_slug, product_slug):
